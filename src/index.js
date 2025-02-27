60bcef6da09640ebda7f0884cc1b249401a96280
Saurabh Yadav <74822612+HakashiKatake@users.noreply.github.com>
2025-02-27 14:19:22 +0530

    handlers

--- src/index.js
+++ src/index.js
@@ -3,11 +3,26 @@
 import dotenv from 'dotenv';
 import mongoose from 'mongoose';
 import connectDB from './db/index.js';
+import { app } from './app.js';
 
 dotenv.config({ path: './env' });   
 
 
-connectDB();
+connectDB()
+.then(() => {
+    app.listen(process.env.PORT || 8000, () => {
+        console.log(`Server is running on port ${process.env.PORT}`);
+    });
+
+    app.on('error', (err) => {
+        console.error(err);
+      });
+}
+)
+.catch((error) => {
+    console.error("MongoDB connection error: ", error);
+}
+);
 
 
 
