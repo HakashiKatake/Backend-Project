ec25bd46c88d6e72f095badfb1e02c0addabeb36
Saurabh Yadav <74822612+HakashiKatake@users.noreply.github.com>
2025-03-03 14:17:01 +0530

    tryna fix stuff

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,6 +1,7 @@
 import { Router } from "express";
 import { registerUser } from "../controllers/user.controller.js";
 import {upload} from "../middlewares/multer.middleware.js"
+
 const router = Router()
 
 router.route("/register").post(
@@ -9,7 +10,7 @@ router.route("/register").post(
             name: "avatar", maxCount: 1
         },
         {
-            name: "cover", maxCount: 1
+            name: "coverImage", maxCount: 1
         }
     ]),
     registerUser)
