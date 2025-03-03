3da251f1398fc92f0cc6fa4abea5619b489a1b61
Saurabh Yadav <74822612+HakashiKatake@users.noreply.github.com>
2025-03-03 12:38:15 +0530

    usercontroller and some fixes

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,7 +1,81 @@
 import { asyncHandler } from "../utils/asyncHandler.js";
-
+import { ApiError } from "../utils/ApiError.js";
+import { User } from "../models/user.model.js";
+import {uploadOnCloudinary} from "../utils/cloudinary.js"
+import { ApiResponse} from "../utils/ApiResponse.js";
 
 const registerUser = asyncHandler( async (req, res) => {
+    //get user details from frontend
+    //validation of user details - not empty, email format, password length
+    //check if user exists
+    //check for images, check for avatar
+    //upload them to cloudinary
+    //create user obj - entry in db
+    //remove pass and refresh token from response
+    //check for user creation
+    //return response
+
+    const { fullname, username, email, password } = req.body;
+    
+    if (
+        [fullname, username, email, password].some((field) => field?.trim() === "")
+    ) {
+        throw new ApiError(400, "Please fill in all fields");
+    }
+
+    const existedUser = User.findOne({
+        $or: [{username}, {email}]
+    })
+
+    if (existedUser) {
+        throw new ApiError(409, "User already exists")
+    }
+
+    const avatarLocalPath = req.files?.avatar[0]?.path;
+    const coverLocalPath = req.files?.cover[0]?.path;
+
+    if (!avatarLocalPath) {
+        throw new ApiError(400, "Please upload an avatar")
+    }
+
+    const avatar = await uploadOnCloudinary(avatarLocalPath)
+    const coverImage = await uploadOnCloudinary(coverLocalPath)
+
+    if(!avatar) {
+        throw new ApiError(500, "Error uploading avatar")
+    }
+
+    const user = await User.create({
+        fullname,
+        avatar: avatar.url,
+        coverImage: coverImage?.url || "",
+        email,
+        password,
+        username: username.toLowerCase()
+    })
+
+    const createdUser = await User.findById(user._id).select("-password -refreshToken")
+    
+    if (!createdUser) {
+        throw new ApiError(500, "Error creating user")
+    }
+
+    return res.status(201).json(
+        new ApiResponse(200, createdUser, "User created successfully")
+    )
+
+    
+
+
+
+
+
+
+
+ 
+
+
+
     res.status(400).json({
         message: "chai aur code"
     })
